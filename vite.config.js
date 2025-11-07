8d81371b513ae83823ada3256151d5725c17e559
Charmaine <thembicharmainem@gmail.com>
2025-11-07 12:48:45 +0200

    Add files via upload
    Added links

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Charmaine-byte/paradise-nursery.git",
+  base: "/paradise-nursery",
   plugins: [react()],
 })
